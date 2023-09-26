494989ba6eb6611dc18e0e7dd23d534bc9929983
damian <suffodamian02@gmail.com>
2023-09-26 02:19:01 -0300

    detalles finales del form de lotes

--- Aplicacion Almacen/Forms/BatchManagerForm.Designer.cs
+++ Aplicacion Almacen/Forms/BatchManagerForm.Designer.cs
@@ -52,9 +52,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
             this.label3.Location = new System.Drawing.Point(11, 310);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.Size = new System.Drawing.Size(82, 17);
             this.label3.TabIndex = 18;
-            this.label3.Text = "ID Destino";
+            this.label3.Text = "ID Producto";
             // 
             // txtBoxIDDestination
             // 
