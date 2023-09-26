676a3ca8e60da503360e59ad6289e0d3110f0241
damian <suffodamian02@gmail.com>
2023-09-26 00:33:43 -0300

    form de lotes

---
+++ Aplicacion Almacen/Forms/BatchManagerForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Aplicacion_Almacen.Forms
+{
+    partial class BatchManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxIDDestination = new System.Windows.Forms.TextBox();
+            this.dateTimePickerBatchShippingDate = new System.Windows.Forms.DateTimePicker();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewBatch = new System.Windows.Forms.DataGridView();
+            this.comboBoxActivated = new System.Windows.Forms.ComboBox();
+            this.txtBoxIDBatch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSearcher = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBatch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(11, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "ID Destino";
+            // 
+            // txtBoxIDDestination
+            // 
+            this.txtBoxIDDestination.Location = new System.Drawing.Point(14, 330);
+            this.txtBoxIDDestination.Name = "txtBoxIDDestination";
+            this.txtBoxIDDestination.Size = new System.Drawing.Size(50, 20);
+            this.txtBoxIDDestination.TabIndex = 17;
+            // 
+            // dateTimePickerBatchShippingDate
+            // 
+            this.dateTimePickerBatchShippingDate.Location = new System.Drawing.Point(14, 264);
+            this.dateTimePickerBatchShippingDate.Name = "dateTimePickerBatchShippingDate";
+            this.dateTimePickerBatchShippingDate.Size = new System.Drawing.Size(216, 20);
+            this.dateTimePickerBatchShippingDate.TabIndex = 16;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(14, 440);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(112, 23);
+            this.buttonRefresh.TabIndex = 15;
+            this.buttonRefresh.Text = "Recargar";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(73, 412);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(53, 23);
+            this.buttonDelete.TabIndex = 14;
+            this.buttonDelete.Text = "Eliminar";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(14, 412);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(53, 23);
+            this.buttonAdd.TabIndex = 13;
+            this.buttonAdd.Text = "Agregar";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(11, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Fecha estimada de entrega";
+            // 
+            // dataGridViewBatch
+            // 
+            this.dataGridViewBatch.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.dataGridViewBatch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewBatch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBatch.Location = new System.Drawing.Point(12, 56);
+            this.dataGridViewBatch.Name = "dataGridViewBatch";
+            this.dataGridViewBatch.Size = new System.Drawing.Size(698, 169);
+            this.dataGridViewBatch.TabIndex = 11;
+            // 
+            // comboBoxActivated
+            // 
+            this.comboBoxActivated.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxActivated.FormattingEnabled = true;
+            this.comboBoxActivated.Location = new System.Drawing.Point(138, 326);
+            this.comboBoxActivated.Name = "comboBoxActivated";
+            this.comboBoxActivated.Size = new System.Drawing.Size(92, 24);
+            this.comboBoxActivated.TabIndex = 41;
+            // 
+            // txtBoxIDBatch
+            // 
+            this.txtBoxIDBatch.Location = new System.Drawing.Point(14, 386);
+            this.txtBoxIDBatch.Name = "txtBoxIDBatch";
+            this.txtBoxIDBatch.Size = new System.Drawing.Size(50, 20);
+            this.txtBoxIDBatch.TabIndex = 42;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(11, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "ID Lote";
+            // 
+            // buttonSearcher
+            // 
+            this.buttonSearcher.Location = new System.Drawing.Point(637, 440);
+            this.buttonSearcher.Name = "buttonSearcher";
+            this.buttonSearcher.Size = new System.Drawing.Size(92, 23);
+            this.buttonSearcher.TabIndex = 44;
+            this.buttonSearcher.Text = "Buscador";
+            this.buttonSearcher.UseVisualStyleBackColor = true;
+            // 
+            // BatchManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(741, 481);
+            this.Controls.Add(this.buttonSearcher);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxIDBatch);
+            this.Controls.Add(this.comboBoxActivated);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxIDDestination);
+            this.Controls.Add(this.dateTimePickerBatchShippingDate);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridViewBatch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BatchManagerForm";
+            this.Text = "BatchManagerForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBatch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxIDDestination;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBatchShippingDate;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewBatch;
+        private System.Windows.Forms.ComboBox comboBoxActivated;
+        private System.Windows.Forms.TextBox txtBoxIDBatch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSearcher;
+    }
+}
