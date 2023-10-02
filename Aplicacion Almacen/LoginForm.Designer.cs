fe290f9d3672876c7055d14914a7ab38e794673c
damian <suffodamian02@gmail.com>
2023-10-02 18:17:56 -0300

    cambios visuales en login

--- Aplicacion Almacen/LoginForm.Designer.cs
+++ Aplicacion Almacen/LoginForm.Designer.cs
@@ -34,6 +34,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelSlidePanelLoginForm = new System.Windows.Forms.Panel();
+            this.buttonMinimize = new System.Windows.Forms.Button();
+            this.buttonCloseMainMenu = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +47,7 @@
             this.buttonLogin.FlatAppearance.BorderSize = 0;
             this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonLogin.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLogin.Location = new System.Drawing.Point(75, 258);
+            this.buttonLogin.Location = new System.Drawing.Point(70, 291);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(159, 28);
             this.buttonLogin.TabIndex = 0;
@@ -55,7 +58,7 @@
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.textBox1.Location = new System.Drawing.Point(75, 158);
+            this.textBox1.Location = new System.Drawing.Point(70, 191);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(159, 20);
             this.textBox1.TabIndex = 1;
@@ -63,7 +66,7 @@
             // textBox2
             // 
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.textBox2.Location = new System.Drawing.Point(75, 220);
+            this.textBox2.Location = new System.Drawing.Point(70, 253);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(159, 20);
@@ -74,7 +77,7 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(72, 126);
+            this.label1.Location = new System.Drawing.Point(67, 159);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 3;
@@ -85,7 +88,7 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.label2.Location = new System.Drawing.Point(72, 191);
+            this.label2.Location = new System.Drawing.Point(67, 224);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 4;
@@ -95,12 +98,46 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.pictureBox1.Image = global::Aplicacion_Almacen.Properties.Resources.logoAzulZennet;
-            this.pictureBox1.Location = new System.Drawing.Point(75, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(70, 45);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(159, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
+            // 
+            // panelSlidePanelLoginForm
+            // 
+            this.panelSlidePanelLoginForm.Location = new System.Drawing.Point(1, 2);
+            this.panelSlidePanelLoginForm.Name = "panelSlidePanelLoginForm";
+            this.panelSlidePanelLoginForm.Size = new System.Drawing.Size(244, 23);
+            this.panelSlidePanelLoginForm.TabIndex = 6;
+            this.panelSlidePanelLoginForm.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelSlidePanelLoginForm_MouseDown);
+            this.panelSlidePanelLoginForm.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelSlidePanelLoginForm_MouseMove);
+            this.panelSlidePanelLoginForm.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelSlidePanelLoginForm_MouseUp);
+            // 
+            // buttonMinimize
+            // 
+            this.buttonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimize.ForeColor = System.Drawing.Color.Olive;
+            this.buttonMinimize.Location = new System.Drawing.Point(278, 2);
+            this.buttonMinimize.Name = "buttonMinimize";
+            this.buttonMinimize.Size = new System.Drawing.Size(20, 23);
+            this.buttonMinimize.TabIndex = 60;
+            this.buttonMinimize.Text = "_";
+            this.buttonMinimize.UseVisualStyleBackColor = true;
+            this.buttonMinimize.Click += new System.EventHandler(this.buttonMinimize_Click);
+            // 
+            // buttonCloseMainMenu
+            // 
+            this.buttonCloseMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCloseMainMenu.ForeColor = System.Drawing.Color.Olive;
+            this.buttonCloseMainMenu.Location = new System.Drawing.Point(251, 2);
+            this.buttonCloseMainMenu.Name = "buttonCloseMainMenu";
+            this.buttonCloseMainMenu.Size = new System.Drawing.Size(20, 23);
+            this.buttonCloseMainMenu.TabIndex = 59;
+            this.buttonCloseMainMenu.Text = "X";
+            this.buttonCloseMainMenu.UseVisualStyleBackColor = true;
+            this.buttonCloseMainMenu.Click += new System.EventHandler(this.buttonCloseMainMenu_Click);
             // 
             // LoginForm
             // 
@@ -108,13 +145,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(84)))), ((int)(((byte)(112)))));
             this.ClientSize = new System.Drawing.Size(310, 343);
+            this.Controls.Add(this.buttonMinimize);
+            this.Controls.Add(this.buttonCloseMainMenu);
+            this.Controls.Add(this.panelSlidePanelLoginForm);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonLogin);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginForm";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -132,6 +172,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelSlidePanelLoginForm;
+        private System.Windows.Forms.Button buttonMinimize;
+        private System.Windows.Forms.Button buttonCloseMainMenu;
     }
 }
 
--- Aplicacion Almacen/LoginForm.cs
+++ Aplicacion Almacen/LoginForm.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -17,10 +18,30 @@ namespace Aplicacion_Almacen
 {
     public partial class LoginForm : Form
     {
+
+        private int m, x, y;
+
         public LoginForm()
         {
             InitializeComponent();
+            roundedCircleForm();
 
+        }
+
+        private void roundedCircleForm()
+        {
+            int radiusBorder = 25;
+
+            Rectangle rectangleBorder = new Rectangle(0, 0, this.Width, this.Height);
+
+            GraphicsPath graphicBorder = new GraphicsPath();
+            graphicBorder.AddArc(rectangleBorder.X, rectangleBorder.Y, radiusBorder, radiusBorder, 180, 90);
+            graphicBorder.AddArc(rectangleBorder.Right - radiusBorder, rectangleBorder.Y, radiusBorder, radiusBorder, 270, 90);
+            graphicBorder.AddArc(rectangleBorder.Right - radiusBorder, rectangleBorder.Bottom - radiusBorder, radiusBorder, radiusBorder, 0, 90);
+            graphicBorder.AddArc(rectangleBorder.X, rectangleBorder.Bottom - radiusBorder, radiusBorder, radiusBorder, 90, 90);
+            graphicBorder.CloseAllFigures();
+
+            this.Region = new Region(graphicBorder);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -93,5 +114,39 @@ namespace Aplicacion_Almacen
                 MessageBox.Show($"Error: {ex.Message}");
             }
         }
+
+        private void panelSlidePanelLoginForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            m = 0;
+        }
+
+        private void panelSlidePanelLoginForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - x, MousePosition.Y - y);
+            }
+        }
+
+        private void panelSlidePanelLoginForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                m = 1;
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void buttonCloseMainMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
     }
 }
